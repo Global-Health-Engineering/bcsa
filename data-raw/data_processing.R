a19f5d1e405852f6ce4734448fb458a6f2819eb5
Lars Schöbitz <lschoebitz@ethz.ch>
2024-03-18 16:42:06 +0100

    remove write functions

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -614,11 +614,6 @@ dictionary <- get_variable_info(data = list(df_aae_exp,
                                             df_pm_trips),
                                 directory = rep("data/", length(file_names)),
                                 file_name = file_names)
-dictionary |>
-  write_csv("data-raw/dictionary.csv")
-
-dictionary |>
-  openxlsx::write.xlsx("data-raw/dictionary.xlsx")
 
 ## using k-means
 
